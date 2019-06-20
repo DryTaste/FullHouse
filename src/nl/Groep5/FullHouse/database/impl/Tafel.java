e3865cc6f90703e2bcc23b5a38934c0dd8d8c0f3
DryTaste <rensvdheuvel@hotmail.com>
2019-06-20 10:13:08 +0200

    added tafel model to tracker

---
+++ src/nl/Groep5/FullHouse/database/impl/Tafel.java
@@ -0,0 +1,31 @@
+package nl.Groep5.FullHouse.database.impl;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Tafel {
+    private int ID, toernooiID;
+
+    private Speler speler;
+
+    public Tafel(int toernooiID) {
+        this.toernooiID = toernooiID;
+    }
+
+    public Tafel(ResultSet resultSet) throws SQLException {
+        this.ID = resultSet.getInt("ID");
+        this.toernooiID = resultSet.getInt("toernooiID");
+    }
+
+    public int getID(){
+        return ID;
+    }
+
+    public int getToernooiID(){
+        return toernooiID;
+    }
+
+    public void setToernooiID(int ID){
+        this.toernooiID = ID;
+    }
+}
